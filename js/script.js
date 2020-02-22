20f016e1b9d10c4ede98aecd7bd2d0090aff5cfa
markoscekic98 <markoscekic998@gmail.com>
2020-02-22 15:20:17 +0100

    Readme added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$(document).ready( () => {
+$(document).ready(() => {
   datumFun();
-////////////template js(for burger menu in navbar)\\\\\\\\\\\\\\\\
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {  // Add a click event on each of them
+  ////////////template js(for burger menu in navbar)\\\\\\\\\\\\\\\\
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0); // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) { // Add a click event on each of them
     $navbarBurgers.forEach($el => {
       $el.addEventListener('click', () => {
         $('.navBarAnchor').css('margin-right', '0px');
@@ -12,13 +12,13 @@ $(document).ready( () => {
         var target = $el.dataset.target;
         var $target = document.getElementById(target);
         $target.style.width = "60%";
-        $target.style.marginLeft = '20%';   // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $target.style.marginLeft = '20%'; // Toggle the class on both the "navbar-burger" and the "navbar-menu"
         $el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
-        });
+      });
     });
   }
-////////////////  End of template js \\\\\\\\\\\\\\\\\\  
+  ////////////////  End of template js \\\\\\\\\\\\\\\\\\  
   document.querySelector('.navbar-burger').addEventListener('click', () => {
     // document.getElementById('navbar').style.backgroundColor = 'rgba(5, 5, 5, 0.4)';
     document.getElementById('navbar').style.backgroundColor = 'rgba(223, 223, 223, 0.4)'; //'rgba(5, 5, 5, 0.4)';
@@ -30,7 +30,7 @@ $(document).ready( () => {
   let count = 0;
   var datumIspis = document.querySelector('#datum');
 
- // datumIspis.addEventListener('click', () => console.log(`Clicked ${count++} times`));
+  // datumIspis.addEventListener('click', () => console.log(`Clicked ${count++} times`));
 
 
   function datumFun() {
@@ -44,60 +44,62 @@ $(document).ready( () => {
       bulmaLogo = `<img src="https://bulma.io/images/made-with-bulma--dark.png" alt="Made with Bulma" width="128" height="24">`;
     }
     document.querySelector('#bulma').innerHTML = bulmaLogo;
- 
+
   }
 
-  
-  if (window.location.pathname == '/products.html' || window.location.pathname == '/Products.html'){
 
-  $.ajax('/data/products.json', {
-    method: "GET",
-    dataType: "json",
-    success: function (products) {
-      product(products);
+  if (window.location.pathname == '/products.html' || window.location.pathname == '/Products.html') {
 
-    } //success
-  }); //ajax
+    $.ajax('/data/products.json', {
+      method: "GET",
+      dataType: "json",
+      success: function (products) {
+        product(products);
 
-  function product(products) {
+      } //success
+    }); //ajax
+
+    function product(products) {
 
       var keyboards = products.filter(kb => kb.product === "keyboard");
-     document.getElementById('bigKeyboardFirst').addEventListener('click',()=> {
-        var bigKeyboardFirst = keyboards.sort((a,b)=> b.numKeys - a.numKeys);
+      document.getElementById('bigKeyboardFirst').addEventListener('click', () => {
+        var bigKeyboardFirst = keyboards.sort((a, b) => b.numKeys - a.numKeys);
         console.log("velike");
         funKeyboardsHtmlDynamic(bigKeyboardFirst);
-      });//EventListener big keyboards
+      }); //EventListener big keyboards
       //////////////// small keyboards first \\\\\\\\\\\\\\\\\\\\\\\\
-      document.getElementById('smallKeyboardFirst').addEventListener('click', ()=> {
-         var smallKeyboardsFirst = keyboards.sort((a,b)=> a.numKeys - b.numKeys);
+      document.getElementById('smallKeyboardFirst').addEventListener('click', () => {
+        var smallKeyboardsFirst = keyboards.sort((a, b) => a.numKeys - b.numKeys);
         console.log("male");
-         funKeyboardsHtmlDynamic(smallKeyboardsFirst);
-      });//eventListner small keyboards
-///////////////  C O L O R \\\\\\\\\\\\\\\\\\\\\\\
+        funKeyboardsHtmlDynamic(smallKeyboardsFirst);
+      }); //eventListner small keyboards
+
+      //// /////////////////////  C O L O R \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
       var color = document.getElementById('clickSelectColor');
-    document.getElementById('clickSelectColor').addEventListener('click',()=>{
-       // e.options[e.selectedIndex].value;
+      document.getElementById('clickSelectColor').addEventListener('click', () => {
+        // e.options[e.selectedIndex].value;
         let selectColor = color.options[color.selectedIndex].value;
-        if(selectColor != 0){
+        if (selectColor != 0) {
           console.log("izabrana boja boju");
-          let keyboardColor = keyboards.filter((kbCol)=> kbCol.color === selectColor);
+          let keyboardColor = keyboards.filter((kbCol) => kbCol.color === selectColor);
           console.log(keyboardColor);
           funKeyboardsHtmlDynamic(keyboardColor);
         }
-        
-      funKeyboardsHtmlDynamic(keyboards);
-        
-});
-   
-    funKeyboardsHtmlDynamic(keyboards);
-//}); //eventListner
-function funKeyboardsHtmlDynamic(data){
-  var htmlAjax = " ";
 
-  let keyboards = data;//.filter(kb => kb.product === "keyboard");
-     keyboards.forEach(p => {
-        htmlAjax +=
-          `  <div class="tiles " >
+        funKeyboardsHtmlDynamic(keyboards);
+
+      });
+
+      funKeyboardsHtmlDynamic(keyboards);
+      //}); //eventListner
+      function funKeyboardsHtmlDynamic(data) {
+        var htmlAjax = " ";
+
+        let keyboards = data; //.filter(kb => kb.product === "keyboard");
+        keyboards.forEach(p => {
+          htmlAjax +=
+            `  <div class="tiles " >
               <div class="tile is-13 keyboardPositioning">
                 <div class="tile is-parent is-6">
                   <article class="tile is-child notification is-dark">
@@ -128,11 +130,11 @@ function funKeyboardsHtmlDynamic(data){
            </div>
         </div>
       </div>`;
-        // <div class="grid is-fluid" style="border:2px solid red;margin:0.5rem;"> <h2>Product category: ${p.product}</h2>// <p>Name: ${p.name}</p>
-        // <p>Number of keys: ${p.numKeys}</p>// <p>Form factor: ${p.formFactor}</p>// <p>Backlit <input type='checkbox' ${p.backlit}></p>// <p>RGB <input type='checkbox'${p.RGB}></p></div>
-      }); //foreach
-      if(data.length<1){
-        htmlAjax = `<main class="hero-body center" style="padding: 2rem;">
+          // <div class="grid is-fluid" style="border:2px solid red;margin:0.5rem;"> <h2>Product category: ${p.product}</h2>// <p>Name: ${p.name}</p>
+          // <p>Number of keys: ${p.numKeys}</p>// <p>Form factor: ${p.formFactor}</p>// <p>Backlit <input type='checkbox' ${p.backlit}></p>// <p>RGB <input type='checkbox'${p.RGB}></p></div>
+        }); //foreach
+        if (data.length < 1) {
+          htmlAjax = `<main class="hero-body center" style="padding: 2rem;">
             <div id="grid" style="width: 768px;">
               <div id="a" class="center-column has-text-centered subtitle" >
                 <h2 class="title is-2 white ">There are no available products</h2><br>
@@ -140,114 +142,114 @@ function funKeyboardsHtmlDynamic(data){
               </div>
             </div>
           </main>`;
-    }
-      let keyboardHTML = document.querySelector("#keyboardHtml");
-      keyboardHTML.innerHTML = htmlAjax;
-    //  keyboardHTML.style.marginTop = '50px';
-  }
- 
-  }//function products 
- } //if products.html
+        }
+        let keyboardHTML = document.querySelector("#keyboardHtml");
+        keyboardHTML.innerHTML = htmlAjax;
+        //  keyboardHTML.style.marginTop = '50px';
+      }
+
+    } //function products 
+  } //if products.html
 
 
   if (window.location.pathname == '/contact.html' || window.location.pathname == '/Contact.html') {
     console.log("Na contact stranici smo");
     document.querySelector('#contactForm').reset(); /// na onLoad stranice se brisu podaci ranije upisano iz forme;
-   document.querySelector('#submitInfo').addEventListener('click', pd => {
-    pd.preventDefault();
-    $.ajax({
-      url: 'https://api.ipify.org?format=jsonp&callback=?',
-      method: "get",
-      dataType: "json",
-      timeout: 3000,
-      complete: data => {
-        let l = JSON.stringify(data).split(':');
-        let lo = l[3].toString();
-        let lok = lo.slice(1, lo.length - 11);
+    document.querySelector('#submitInfo').addEventListener('click', pd => {
+      pd.preventDefault();
+      $.ajax({
+        url: 'https://api.ipify.org?format=jsonp&callback=?',
+        method: "get",
+        dataType: "json",
+        timeout: 3000,
+        complete: data => {
+          let l = JSON.stringify(data).split(':');
+          let lo = l[3].toString();
+          let lok = lo.slice(1, lo.length - 11);
           document.cookie = `IP=${lok}`;
         },
-      error: function (error, xhr, status) {
-        console.log(status);
-        alert("Please disable your adblock");
-      }
-    });
-    
-    var data = [];
-    var validation = true;
-    ///////// f name ////////
-         var fname = document.querySelector('#fname').value.trim();
-         var fnameError = document.querySelector('#fnameError');
-         var reExName = /^[A-Z]{1}[a-z]{1,13}$/;
-         if (fname.length < 2) {
-           fnameError.innerHTML = `Please enter your first name`;
-           validation = false;
-         }
-         if (!reExName.test(fname)) {
-          fnameError.innerHTML = `Please enter your first name correctly`;
-          validation = false;
+        error: function (error, xhr, status) {
+          console.log(status);
+          alert("Please disable your adblocker......");
         }
-         data.push(fname);
-          document.cookie = `First name = ${fname}`;
-        
-     
-         ///////////////// l name \\\\\\\\\\\\\    
-     
+      });
+
+      var data = [];
+      var validation = true;
+      ///////// f name ////////
+      var fname = document.querySelector('#fname').value.trim();
+      var fnameError = document.querySelector('#fnameError');
+      var reExName = /^[A-Z]{1}[a-z]{1,13}$/;
+      if (fname.length < 2) {
+        fnameError.innerHTML = `Please enter your first name`;
+        validation = false;
+      }
+      if (!reExName.test(fname)) {
+        fnameError.innerHTML = `Please enter your first name correctly`;
+        validation = false;
+      }
+      data.push(fname);
+      document.cookie = `First name = ${fname}`;
+
+
+      ///////////////// l name \\\\\\\\\\\\\    
+
       var lname = document.querySelector('#lname').value.trim();
-     var lnameError = document.querySelector('#lnameError');
-     if(lname.length<1){
-       lnameError.innerHTML =`Please enter your last name corretly`;
-       validation = false;
-     }
-     if (!reExName.test(lname)) {
-      lnameError.innerHTML = `Please enter your last name correctly`;
-      validation = false;
-     }
-     data.push(lname);
-     document.cookie = `Last name = ${lname}`;
-     
-     //////////////   z  I  P   \\\\\\\\\\\\\\\\\\ 
-     
+      var lnameError = document.querySelector('#lnameError');
+      if (lname.length < 1) {
+        lnameError.innerHTML = `Please enter your last name corretly`;
+        validation = false;
+      }
+      if (!reExName.test(lname)) {
+        lnameError.innerHTML = `Please enter your last name correctly`;
+        validation = false;
+      }
+      data.push(lname);
+      document.cookie = `Last name = ${lname}`;
+
+      //////////////   z  I  P   \\\\\\\\\\\\\\\\\\ 
+
       var zip = document.querySelector('#zip').value.trim();
       var zipError = document.querySelector('#zipError');
-      if(zip<10000 || zip>99999){
+      if (zip < 10000 || zip > 99999) {
         zipError.innerHTML = `Please enter correct zip code (10 000 - 99 999)`;
         validation = false;
       }
       data.push(zip);
       document.cookie = `Zip Code = ${zip}`;
-      
+
       /////////////////  E M A I L \\\\\\\\\\\\\\\
-      
+
       var em = document.querySelector('#email').value.trim();
       var emError = document.querySelector('#emailError');
       var reExEmail = /^[a-z0-9._%+]+@[a-z0-7.]+\.[a-z]{2,4}$/;
-      if(em.length<1){
-        emError.innerHTML =`Please enter your email address`;
+      if (em.length < 1) {
+        emError.innerHTML = `Please enter your email address`;
         validation = false;
       }
-      if(!reExEmail.test(em)){
+      if (!reExEmail.test(em)) {
         emError.innerHTML = `Please enter valid email address`;
         validation = false;
       }
       data.push(em);
-      document.cookie  = `Email = ${em}`;
+      document.cookie = `Email = ${em}`;
       ///////////////////////  PassWord \\\\\\\\\\\\\\\\\
       var pass = document.querySelector('#pass').value.trim();
       var passError = document.querySelector('#passError');
       var reExPass = /^[a-z0-9]{5,20}$/;
-      if(!reExPass.test(pass)){
+      if (!reExPass.test(pass)) {
         passError.innerHTML = `Please enter alfanumeric only password between 5 and 20 character in length`;
         validation = false;
       }
       let enigma = CryptoJS.AES.encrypt(pass, '11231').toString();
       data.push(enigma);
-      document.cookie= `Password = ${enigma}`;
+      document.cookie = `Password = ${enigma}`;
 
 
-     if(data.length>2 && validation){
-       console.log(data);
-       alert("Successful login, thank you");
-     } 
+      if (data.length > 2 && validation) {
+        console.log(data);
+        alert("Successful login, thank you");
+      }
     });
-  }//if contact
+  } //if contact
 }); //document.ready
---
+++ readme.md
@@ -0,0 +1,18 @@
+ This website is for school project for course 'Web programiranje 2' 
+
+ Course requires the following:
+  - Website must be responsive  -check
+  - SEO -in progress
+  - Form data validation with regular expression -check
+  - Ajax & JSON -check
+  - Filter & Sort of JSON data -check
+  - Cookies & Local Storage - cookies:check, LS in progress
+  
+Frameworks, libraries, API's, CDS used:
+ CDN: CloudFlare, JSdeliver
+ Framework: Bulma (CSS) 
+ API: ipify
+ Libraries: jQuery(JavaScript)
+
+Privacy policy: Website doesnt not share any personal or any other information.
+
